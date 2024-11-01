fa7d2f05d95fe9484a0c750bec6d769e244c4bf9
ethrad <asdf123f4f4@gmail.com>
2024-11-01 19:19:47 +0900

    init commit

---
+++ src/main/java/com/sparta/outsourcing_nt/OutsourcingNtApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.outsourcing_nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingNtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsourcingNtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/outsourcing_nt/OutsourcingNtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.outsourcing_nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingNtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
