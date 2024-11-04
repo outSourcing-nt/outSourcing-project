0ea88bc5e7e2630385fabc99d3235160abc5acdd
musicmalgon <musicmalgon@gmail.com>
2024-11-04 16:14:47 +0900

    feat: crud 추가

--- src/main/java/com/sparta/outsourcing_nt/controller/MenuController.java
+++ src/main/java/com/sparta/outsourcing_nt/controller/MenuController.java
@@ -1,13 +1,49 @@
 package com.sparta.outsourcing_nt.controller;
 
+import com.sparta.outsourcing_nt.dto.menu.req.MenuRequestDto;
+import com.sparta.outsourcing_nt.dto.menu.res.MenuResponseDto;
+import com.sparta.outsourcing_nt.dto.menu.res.MenuResponsePage;
 import com.sparta.outsourcing_nt.service.MenuService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/store/{storeId}/menu")
 public class MenuController {
     private final MenuService menuService;
+
+    @PostMapping
+    public ResponseEntity<MenuResponseDto> createMenu(@RequestBody MenuRequestDto requestDto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(menuService.createMenu(requestDto));
+    }
+
+    @GetMapping()
+    public ResponseEntity<MenuResponsePage> getAllMenus(@RequestParam(required = false, defaultValue = "0") int page,
+                                                        @RequestParam(required = false, defaultValue = "10") int size,
+                                                        @RequestParam(required = false, defaultValue = "modifiedAt") String criteria){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(menuService.getAllMenus(page, size, criteria));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MenuResponseDto> updateMenu(@PathVariable Long id, @RequestBody MenuRequestDto requestDto){
+        menuService.updateMenu(id, requestDto);
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MenuResponseDto> deleteMenu(@PathVariable Long id){
+        menuService.deleteMenu(id);
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
 }
--- src/main/java/com/sparta/outsourcing_nt/repository/MenuRepository.java
+++ src/main/java/com/sparta/outsourcing_nt/repository/MenuRepository.java
@@ -2,6 +2,8 @@ package com.sparta.outsourcing_nt.repository;
 
 import com.sparta.outsourcing_nt.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
